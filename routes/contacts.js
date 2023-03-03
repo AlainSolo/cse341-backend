398b3aa07f9273ea68d7ad3ac0b9807186752bbd
Alain <solohery1978@gmail.com>
2023-03-03 21:30:56 +0700

    commit 3 (lesson 3)

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,9 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createInfo);
+
+router.put('/:id', contactsController.updateInfo);
+
+router.delete('/:id', contactsController.deleteInfo);
 module.exports = router;
